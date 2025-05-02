abe8b636c88410364303348b74826932c2d15cc0
empchief <bjornmagnekristensen1999@gmail.com>
2025-05-02 09:28:04 +0200

    Just made a request to facebook, nothing changed just commiting so can switch from laptop to desktop

--- fbgraphplay/main.py
+++ fbgraphplay/main.py
@@ -1,5 +1,10 @@
 from dotenv import load_dotenv
 import os
+import requests
 load_dotenv()
 
 token = os.getenv("FACEBOOK_ACCSESS_TOKEN")
+url = f"https://graph.facebook.com/v22.0/me?fields=id%2Cname&access_token={token}"
+
+req = requests.get(url)
+print(req.content)
