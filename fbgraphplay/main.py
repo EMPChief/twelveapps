70d3e2b3241f60ff37852fda5f1f75e8dde7c868
empchief <bjornmagnekristensen1999@gmail.com>
2025-05-03 23:43:44 +0200

    Played a bit with fb graph fetching post and looking what i posted, tried to delete all my old post but seems that need to get permission in token and can't find it on the graph docs so will pause for now

--- fbgraphplay/main.py
+++ fbgraphplay/main.py
@@ -1,10 +1,20 @@
 from dotenv import load_dotenv
 import os
 import requests
+
 load_dotenv()
 
-token = os.getenv("FACEBOOK_ACCSESS_TOKEN")
-url = f"https://graph.facebook.com/v22.0/me?fields=id%2Cname&access_token={token}"
+token = os.getenv("FACEBOOK_ACCESS_TOKEN")
+page_id = "30088280604092515"
+url = f"https://graph.facebook.com/v22.0/{page_id}?fields=id,posts{{message,story,created_time}}&access_token={token}"
 
-req = requests.get(url)
-print(req.content)
+response = requests.get(url)
+data = response.json()
+
+if "posts" in data:
+    for post in data["posts"]["data"]:
+        print(post.get("message") or post.get("story"))
+        print(post.get("created_time"))
+        print("-" * 40)
+else:
+    print("No posts found or an error occurred.")
